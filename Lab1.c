6c11733dd43a59ebdebb2bc028e4ca1a3a310402
SeanMongru <31765998+SeanMongru@users.noreply.github.com>
2017-10-05 17:17:06 -0400

    Add files via upload

---
+++ Lab1.c
@@ -0,0 +1,18 @@
+/* Lab 1 first file - ID number 96013952 */
+#include <p18f452.h>
+/* Set configuration bits for use with ICD2 */
+#pragma config OSC = HS
+#pragma config WDT = OFF
+#pragma config LVP = OFF
+unsigned char counter;  //specify counter as unsigned char
+void main (void)
+{
+    TRISB = 0;
+    counter = 1;
+    
+    while(counter < 16){
+        PORTB = counter;
+        counter += 1;
+    }
+    
+}
